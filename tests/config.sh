b10bcac1bc9d3466c2a7a6d64636a62d846ae894
Bryan Hann <bhann@pobox.com>
2019-07-28 15:10:51 +0800

    Add testing framework and initial tests.

---
+++ tests/config.sh
@@ -0,0 +1,41 @@
+function test_begin {
+	FAILED=0
+	TESTED=0
+}
+
+function test_end {
+	echo "Ran $TESTED test(s) with $FAILED failures(s)."
+}
+
+
+function backspace {
+echo -e -n "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b"
+echo -e -n "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b"
+echo -e -n "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b"
+}
+
+function info {
+	echo [info] $*
+}
+
+function return_with {
+	return $1
+}
+
+function runtest {
+	des=$1
+	cmd=$2	
+	exp=$3
+	echo -n [....] $des
+	eval $cmd
+	got=$?
+	#echo -n "   exp:[$exp] got:[$got]"
+	backspace
+	#sleep 1
+	export TESTED=$(( $TESTED + 1 ))
+	[ "$exp" == "$got" ] && echo "[ ok ]" && return 0
+	echo "[fail]"
+	export FAILED=$(( $FAILED + 1 ))
+}
+
+
